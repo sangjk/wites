8c98afe969c5f7d0f474b5c6c1e6a615b4f26690
youtube-科普 <sangkl@163.com>
2021-09-29 23:46:33 +0800

    Add files via upload

---
+++ index_files/config.php
@@ -0,0 +1,7 @@
+<?PHP
+//匿名网址源码
+//Settings
+$url = "localhost:8080/anonym_jb51/demos/anonym"; //程序网址,不带http://结尾不带反斜线/：如 (http://www.ccw8.com/ :www.ccw8.com - 子目录：http://www.ccw8.com/demos/ :www.ccw8.com/demos)
+$sitename = "匿名网址"; //网站名称
+$wait = "15"; //跳转等待时间（单位：秒）
+?>
